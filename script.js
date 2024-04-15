90695e8b358fdc87dc33d0b953350d76516de84c
tmntee <107428229+tmntee@users.noreply.github.com>
2024-04-14 22:25:04 -0400

    Add functionality to clear and equals buttons

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function multiply(num1, num2)
 
 function divide(num1, num2)
 {
-    return num1 / num2;
+    return Math.round(num1 / num2);
 }
 
 function operate(num1, op, num2)
@@ -29,6 +29,10 @@ function operate(num1, op, num2)
         case 'multiply':
             return multiply(num1, num2);
         case 'divide':
+            if (num2 === 0)
+            {
+                return 'erm impossible';
+            }
             return divide(num1, num2);
     }
 }
@@ -36,19 +40,23 @@ function operate(num1, op, num2)
 let firstNumber;
 let operator;
 let secondNumber;
+let isDisplayingResult;
 
 const displayScreen = document.querySelector('#calculator-screen');
-console.log(displayScreen);
 
 let displayValue = displayScreen.textContent;
 function displayOnScreen(content)
-{
+{   
+    if (isDisplayingResult)
+    {
+        displayScreen.textContent = "";
+        isDisplayingResult = false;
+    }
     displayScreen.textContent += content;
     displayValue = parseInt(displayScreen.textContent);
 }
 
 const numericButtons = Array.from(document.querySelectorAll('#calculator-row button#num-button'));
-console.log(numericButtons);
 
 numericButtons.map((obj) => obj.addEventListener("click", () => displayOnScreen(obj.textContent)));
 
@@ -56,8 +64,7 @@ function operatorButton(typeOfOperator)
 {
     operator = typeOfOperator;
     firstNumber = displayValue;
-    displayScreen.textContent = "    ";
-    console.log(operator);
+    displayScreen.textContent = "";
     console.log(firstNumber);
 }
 
@@ -69,10 +76,24 @@ operatorButtons[2].addEventListener("click", () => operatorButton("multiply"));
 operatorButtons[3].addEventListener("click", () => operatorButton("divide"));
 
 function equals()
-{
+{   
     secondNumber = displayValue;
-    displayScreen.textContent = operate(firstNumber, operator, secondNumber);
+    displayScreen.textContent = operate(firstNumber, operator, secondNumber)
+    displayValue = parseInt(displayScreen.textContent);
+    isDisplayingResult = true;
+    console.log(secondNumber);
 }
 
 const equalsButton = document.querySelector('button#equals-button');
 equalsButton.addEventListener("click", () => equals());
+
+function resetCalculator()
+{
+    firstNumber = '';
+    operator = '';
+    secondNumber = '';
+    displayScreen.textContent = '';
+    displayValue = '';
+}
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => resetCalculator());
