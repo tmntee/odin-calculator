3c116afe7cfd55143bb6f4c9a5df686c498676e5
tmntee <107428229+tmntee@users.noreply.github.com>
2024-04-14 20:54:27 -0400

    Add base operation functions

---
+++ script.js
@@ -0,0 +1,38 @@
+function add(num1, num2)
+{
+    return num1 + num2;
+}
+
+function subtract(num1, num2)
+{
+    return num1 - num2;
+}
+
+function multiply(num1, num2)
+{
+    return num1 * num2;
+}
+
+function divide(num1, num2)
+{
+    return num1 / num2;
+}
+
+let firstNum;
+let operator;
+let secondNum;
+
+function operate(num1, op, num2)
+{
+    switch (op)
+    {
+        case 'add':
+            return add(num1, num2);
+        case 'subtract':
+            return subtract(num1, num2);
+        case 'multiply':
+            return multiply(num1, num2);
+        case 'divide':
+            return divide(num1, num2);
+    }
+}
